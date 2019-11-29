0670c189461b134f95023843e6acbfceb6c8a9c8
Jck-eN <jaceknit.rac@gmail.com>
2019-11-29 19:59:48 +0100

    Added JSON Parser

---
+++ src/main/agh/cs/project1/Config.java
@@ -0,0 +1,18 @@
+package agh.cs.project1;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
+public class Config {
+    public static Integer EVOLUTION_MAP_WIDTH = 100;
+    public static Integer EVOLUTION_MAP_HEIGHT = 30;
+    public static Integer JUNGLE_WIDTH = 10;
+    public static Integer JUNGLE_HEIGHT = 10;
+    public static Integer PLANT_MAX_SIZE = 10;
+    public static Integer PLANT_DEFAULT_SIZE = 5;
+    public static Integer ENERGY_LOST_PER_MOVE = 1;
+    public static Integer REQUIRED_ENERGY = 10;
+    public static Integer DEFAULT_ENERGY = 400;
+}
--- src/main/agh/cs/project1/World.java
+++ src/main/agh/cs/project1/World.java
@@ -1,10 +1,26 @@
 package agh.cs.project1;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
 import java.io.IOException;
 
 public class World {
     public static void main(String[] args) {
         try {
+
+            Object obj = new JSONParser().parse(new FileReader("D:\\Jacek\\Programowanie\\AGH\\AGH_3_JAVA\\labs\\src\\main\\agh\\cs\\project1\\config.json"));
+            JSONObject jo = (JSONObject) obj;
+            Config.EVOLUTION_MAP_WIDTH = Math.toIntExact((Long) jo.get("width"));
+            Config.EVOLUTION_MAP_HEIGHT = Math.toIntExact((Long) jo.get("height"));
+            Config.DEFAULT_ENERGY = Math.toIntExact((Long)  jo.get("startEnergy"));
+            Config.ENERGY_LOST_PER_MOVE = Math.toIntExact((Long) jo.get("moveEnergy"));
+            Config.PLANT_DEFAULT_SIZE = Math.toIntExact((Long) jo.get("startPlantSize"));
+            Config.PLANT_MAX_SIZE = Math.toIntExact((Long) jo.get("maxPlantSize"));
+            Config.JUNGLE_HEIGHT = Math.toIntExact(Math.round(Double.valueOf(Config.EVOLUTION_MAP_HEIGHT) * (Double) jo.get("jungleRatio")));
+            Config.JUNGLE_WIDTH = Math.toIntExact(Math.round(Double.valueOf(Config.EVOLUTION_MAP_WIDTH) * (Double) jo.get("jungleRatio")));
 
             EvolutionMap map = new EvolutionMap();
             //    IWorldMap map = new RectangularMap(10, 5);
@@ -37,6 +53,8 @@ public class World {
         catch (InterruptedException ex2){
             System.out.println(ex2.getMessage());
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
 
